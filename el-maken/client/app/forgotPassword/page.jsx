797f169d7134d352361abaee1985ff98c36e133c
MoNourTrabelsi <trabelsimednour@gmail.com>
2023-11-11 21:36:26 +0100

    repo is ready

--- el-maken/client/app/forgotPassword/page.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import FPComp from '@/components/forgotPassword-components/FPComp'
-const forgotPassword = () => {
-  return (
-    <div><FPComp /></div>
-  )
-}
-
-export default forgotPassword
